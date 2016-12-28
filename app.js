9466af9bb002762defbb5cd61770877c778ad4e4
Lukas (thevukaslt) <vngs.lks@gmail.com>
2016-12-28 11:44:36 +0200

    Testing

--- app.js
+++ app.js
@@ -1,70 +1,92 @@
-
-
 (function(){
 
 
-	angular.module('app', ['ngMaterial', 'ngRoute'])
-
-	.controller('SongsController', function() {
-		this.songs = library;
-	})
-
-	.controller('AppController', function() {
-
-	})
+	angular.module('app', ['ngRoute'])
 
 	// configure our routes
-    .config(function($routeProvider) {
+    .config(function($routeProvider, $locationProvider) {
         $routeProvider
 
             // route for the home page
             .when('/', {
                 templateUrl : 'pages/home.html',
-                controller  : 'mainController'
+                controller  : 'mainController',
+                controllerAs: 'main'
             })
 
             // route for the about page
             .when('/about', {
                 templateUrl : 'pages/about.html',
-                controller  : 'aboutController'
+                controller  : 'aboutController',
+                controllerAs: 'about'
             })
 
-            // route for the contact page
-            .when('/contact', {
-                templateUrl : 'pages/contact.html',
-                controller  : 'contactController'
-            });
+            // route for the song page
+            .when('/song/:id', {
+                templateUrl : 'pages/song.html',
+                controller  : 'songController',
+                controllerAs: 'song'
+            })
+            //fallback URL address
+            .otherwise({ redirectTo: '/' });;
+
+        // use the HTML5 History API
+    	$locationProvider.html5Mode(true);
+
+            
     })
 
-    // create the controller and inject Angular's $scope
-    .controller('mainController', function($scope) {
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
+    .run(
+        function($http, $rootScope) {
+            
+            $http.get('edeno-aidai.json').then(
+
+                function(response){
+                    console.log(response.statusText);
+                    $rootScope.library = response.data;
+                },
+
+                function(response){
+                    //console.log();
+                    console.log('Klaida '+response.status+', biblioteka neįkelta.');
+                }
+           );
+
+
+        }
+
+    )
+
+    .controller('mainController', function($scope, $http, $rootScope) {
+    	var main = this;
+
+    	main.songs = $rootScope.library;
+
     })
 
     .controller('aboutController', function($scope) {
         $scope.message = 'Look! I am an about page.';
     })
 
-    .controller('contactController', function($scope) {
-        $scope.message = 'Contact us! JK. This is just a demo.';
+    //Song controller
+    .controller('songController', function($rootScope, $routeParams, $filter, $sce) {
+ 		var self = this;
+
+        //Find hymn
+        self.data = $filter('filter')($rootScope.library, {id: $routeParams.id})[0];
+        //Trust body for HTML output
+        self.data.body = $sce.trustAsHtml(self.data.body);
+        self.data.copyright = $sce.trustAsHtml(self.data.copyright);
+
+
+    })
+
+    //Navigation controller
+    .controller('navCtrl', function($scope, $location) {
+    	this.url = function (path) {
+	    	return $location.path() == path;
+	    };
+
     });
-
-
-
-
-
-	var library = [
-		{ 
-			title: 'Giesmė vienas', 
-			number: 1 ,
-			body: "Tu esi"
-		},
-		{ 
-			title: 'Antroji giesmė', 
-			number: 2 ,
-			body: "Aš esu"
-		}
-
-	];
+	
 })();
