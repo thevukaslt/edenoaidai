727305a1c4ec6ee564d655801b75768c8888d0a6
Lukas <vngs.lks@gmail.com>
2019-04-06 22:13:15 +0200

    Add global styles

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,39 @@
     <div id="app">
         <div id="nav">
             <router-link to="/">Home</router-link>|
-            <router-link to="/about">About</router-link>
+            <router-link to="/song/2">Song</router-link>
         </div>
-        <router-view />
+        <router-view/>
     </div>
 </template>
 
 <style lang="scss">
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-#nav {
-    padding: 30px;
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-        &.router-link-exact-active {
-            color: #42b983;
+    body {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+
+        padding: 0 15px;
+        margin: 0;
+    }
+    #app {
+        display: grid;
+
+        height: 100vh;
+
+        grid-template-rows: auto 1fr;
+        grid-template-columns: 1fr;
+
+        justify-items: center;
+    }
+    #nav {
+        padding: 30px;
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+            &.router-link-exact-active {
+                color: #42b983;
+            }
         }
     }
-}
 </style>
