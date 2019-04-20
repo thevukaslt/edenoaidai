88619a65d806c977fe7100ec2b9c0a66513b6b5f
Lukas <lukas@youtuberis.lt>
2019-04-20 21:48:52 +0200

    [wip] bottom navbar

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,42 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/">Home</router-link>|
-            <router-link to="/song/2">Song</router-link>
-        </div>
+    <main>
         <router-view/>
-    </div>
+        <navbar></navbar>
+    </main>
 </template>
 
+<script>
+    import Navbar from './components/Navbar.vue';
+    export default {
+        components: {
+            Navbar,
+        },
+    };
+</script>
+
+
 <style lang="scss">
+    html,
     body {
+        height: 100%;
+    }
+    body {
+        display: flex;
+        flex-direction: column;
+
+        align-items: center;
+
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
 
-        padding: 0 15px;
         margin: 0;
     }
-    #app {
-        display: grid;
+    main {
+        flex: 1;
+        display: flex;
 
-        height: 100vh;
-
-        grid-template-rows: auto 1fr;
-        grid-template-columns: minmax(290px, 1fr);
-
-        justify-items: center;
-    }
-    #nav {
-        padding: 30px;
-        a {
-            font-weight: bold;
-            color: #2c3e50;
-            &.router-link-exact-active {
-                color: #42b983;
-            }
-        }
+        padding: 0 15px;
+        padding-top: 30px;
     }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,81 @@
+<template>
+    <footer>
+        <ul class="menu">
+            <li>
+                <router-link to="/" class="menu__item">
+                    <span class="icon-home"></span>
+                    Titulinis
+                </router-link>
+            </li>
+            <li>
+                <router-link to="/search" class="menu__item">
+                    <span class="icon-search"></span>
+                    Paieška
+                </router-link>
+            </li>
+
+            <li>
+                <router-link to="/star" class="menu__item">
+                    <span class="icon-star"></span>
+                    Pamėgti
+                </router-link>
+            </li>
+        </ul>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'navbar',
+        mounted() {
+            document.body.insertBefore(
+                this.$el,
+                document.querySelector('main').nextSibling,
+            );
+        },
+    };
+</script>
+
+<style lang="scss">
+    footer {
+        width: 100%;
+
+        border-top: 1px solid rgba(0, 0, 0, 0.1);
+        box-shadow: 0 -8px 10px 0px rgba(0, 0, 0, 0.1);
+
+        .menu {
+            display: flex;
+
+            justify-content: space-around;
+
+            margin: 0;
+            padding: 0;
+            list-style: none;
+
+            &__item {
+                display: flex;
+                flex-direction: column;
+
+                align-items: center;
+
+                color: rgba(175, 170, 185, 1);
+                text-decoration: none;
+
+                padding: 10px;
+
+                :first-child {
+                    margin-bottom: 5px;
+                    font-size: 20px;
+                }
+
+                &.is-active {
+                    color: rgba(165, 36, 61, 1);
+                }
+
+                &:hover {
+                    color: rgba(165, 36, 61, 0.5);
+                }
+            }
+        }
+    }
+</style>
