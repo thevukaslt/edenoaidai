cb85d29ba439ac16e314ecf091b1f265c4eae5cd
Lukas <lukas@youtuberis.lt>
2019-05-04 20:15:50 +0200

    Add favorite button to single view

--- src/views/Single.vue
+++ src/views/Single.vue
@@ -1,7 +1,21 @@
 <!-- eslint-disable vue/no-v-html -->
 <template>
     <div v-if="song" class="song">
-        <h2 class="song__title">{{ song.songId }} {{ song.title }}</h2>
+        <h2 class="song__title">
+            {{ song.songId }} {{ song.title }}
+            <button
+                class="song__favorite-button"
+                :title="song.favorited ? 'Išsaugota' : 'Pamėgti'"
+                @click="toggleFavorite"
+            >
+                <svg v-if="song.favorited" class="icon">
+                    <use xlink:href="#icon-star-full"></use>
+                </svg>
+                <svg v-else class="icon">
+                    <use xlink:href="#icon-star-empty"></use>
+                </svg>
+            </button>
+        </h2>
         <p class="song__verse">
             <em>{{ song.verse }}</em>
         </p>
@@ -36,6 +50,24 @@
                     console.error(err.message);
                 });
         },
+        methods: {
+            toggleFavorite() {
+                this.$songs
+                    .update(this.song.id, {
+                        favorited: this.song.favorited ? 0 : 1,
+                    })
+                    .then(successfullyUpadated => {
+                        if (successfullyUpadated) {
+                            this.song.favorited = this.song.favorited ? 0 : 1;
+                        } else {
+                            console.info('Failed to toggle favorite');
+                        }
+                    })
+                    .catch(err =>
+                        console.error(`Error toggling favorite status ${err}`),
+                    );
+            },
+        },
     };
 </script>
 
@@ -63,6 +95,22 @@
             font-size: 16px;
 
             margin-bottom: 20px;
+        }
+
+        &__favorite-button {
+            display: inline-block;
+
+            font-size: 20px;
+            line-height: 1;
+
+            vertical-align: bottom;
+
+            padding: 0 10px;
+
+            border: none;
+
+            color: rgba(228, 179, 99, 1);
+            background-color: transparent;
         }
     }
 </style>
