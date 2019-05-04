8df3adb470e354eaea5cd6587b28b31a2f1a1815
Lukas <lukas@youtuberis.lt>
2019-05-04 09:18:44 +0200

    Offset by random songs on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@
         created() {
             this.$songs
                 .orderBy('id')
-                .offset(67)
+                .offset(Math.round(Math.random() * 100))
                 .limit(10)
                 .toArray()
                 .then(songs => {
