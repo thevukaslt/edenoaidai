ad0b09aec0d53143e899cb33ce0f9a3af84b5075
Lukas <lukas@youtuberis.lt>
2019-05-04 21:03:34 +0200

    Add Sentry error tracking

--- src/main.js
+++ src/main.js
@@ -12,3 +12,7 @@ new Vue({
     router,
     render: h => h(App),
 }).$mount('main');
+
+if ('Sentry' in window && process.env.NODE_ENV === 'production') {
+    window.Sentry.init({ dsn: 'https://d4fcda1e38c442a2a518daf99390583e@sentry.io/1452482' });
+}
