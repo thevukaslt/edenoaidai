ab4545765733f2d57d0873f7f8edf9df2673771e
Lukas <lukas@youtuberis.lt>
2019-05-17 12:28:39 +0200

    Allow changing font size for song lyrics

--- src/views/Single.vue
+++ src/views/Single.vue
@@ -16,10 +16,14 @@
                 </svg>
             </button>
         </h2>
+        <div class="song__buttons">
+            <button @click="adjustFontSize(true)" class="font-size-button">Aa++</button>
+            <button @click="adjustFontSize(false)" class="font-size-button">Aa--</button>
+        </div>
         <p class="song__verse">
             <em>{{ song.verse }}</em>
         </p>
-        <div class="song__body" v-html="song.body"></div>
+        <div class="song__body" v-html="song.body" :style="fontSizeStyle"></div>
         <small class="song__copyright" v-html="song.copyright"></small>
     </div>
 </template>
@@ -36,6 +40,12 @@
         data() {
             return {
                 song: {},
+                get fontSize() {
+                    return parseInt(localStorage.getItem('fontSize')) || 16;
+                },
+                set fontSize(newVal) {
+                    localStorage.setItem('fontSize', newVal);
+                },
             };
         },
         created() {
@@ -49,6 +59,13 @@
                 .catch(err => {
                     console.error(err.message);
                 });
+        },
+        computed: {
+            fontSizeStyle: function() {
+                return {
+                    fontSize: `${this.fontSize}px`,
+                };
+            },
         },
         methods: {
             toggleFavorite() {
@@ -66,6 +83,9 @@
                     .catch(err =>
                         console.error(`Error toggling favorite status ${err}`),
                     );
+            },
+            adjustFontSize(increase) {
+                increase ? (this.fontSize += 1) : (this.fontSize -= 1);
             },
         },
     };
@@ -111,6 +131,26 @@
 
             color: rgba(228, 179, 99, 1);
             background-color: transparent;
+        }
+
+        &__buttons {
+            display: flex;
+
+            padding: 10px 0;
+
+            .font-size-button {
+                font-size: 16px;
+                line-height: 1.5;
+
+                border: none;
+                border-radius: 20px;
+                background-color: white;
+                box-shadow: 1px 1px 3px 0 rgba(0, 0, 0, 0.5);
+
+                &:not(:last-child) {
+                    margin-right: 10px;
+                }
+            }
         }
     }
 </style>
